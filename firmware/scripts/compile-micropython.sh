6053333f2bf0aad1b3db13e18c789c219be5b984
Le Roux Bodenstein <lerouxb@gmail.com>
2023-07-28 15:58:55 +0100

    wip

--- firmware/scripts/compile-micropython.sh
+++ firmware/scripts/compile-micropython.sh
@@ -1,9 +1,11 @@
 # remember to set the PORT env var to point to your serial port
+# also, you want to be running this from ~/src/micropython/ports/esp32
 
 #make clean BOARD=GENERIC_SPIRAM
 
 make -j 4 DEBUG=1 BOARD=GENERIC_SPIRAM USER_C_MODULES=../../../../gc9a01_mpy/src/micropython.cmake
 
+# you only have to erase if you want to replace the python code
 make BOARD=GENERIC_SPIRAM USER_C_MODULES=../../../../gc9a01_mpy/src/micropython.cmake erase
 
 make BOARD=GENERIC_SPIRAM USER_C_MODULES=../../../../gc9a01_mpy/src/micropython.cmake deploy
--- firmware/scripts/copy-code.sh
+++ firmware/scripts/copy-code.sh
@@ -1,10 +1,13 @@
 # remember to set the PORT env var to point to your serial port
 
-mpremote connect ${PORT} cp -r utils :
-mpremote connect ${PORT} cp -r apps :
-mpremote connect ${PORT} cp -r gui :
-mpremote connect ${PORT} cp -r system :
-mpremote connect ${PORT} cp main.py :
+mpremote connect ${PORT} cp -r src/utils :
+mpremote connect ${PORT} cp -r src/apps :
+mpremote connect ${PORT} cp -r src/gui :
+mpremote connect ${PORT} cp -r src/system :
+mpremote connect ${PORT} cp src/main.py :
+
+# temporary
+mpremote connect ${PORT} cp scripts/time2.py :
 
 DT=$(python3 -c "from datetime import datetime; print(datetime.now().strftime('(%Y, %m, %d, 0, %H, %M, %S, 0)'))")
 mpremote connect ${PORT} exec "from machine import RTC; rtc = RTC(); rtc.datetime($DT)"
